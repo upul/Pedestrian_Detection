0e28d87f12df0bac5b0cde84e3d8d052eb878287
Upul Bandara <upulbandara@gmail.com>
2017-07-12 08:51:48 -0500

    image reading utilities

--- utils.py
+++ utils.py
@@ -2,9 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import os
-import re
 
-FILE_EXTRACTION_RE = re.compile(r'([a-zA-Z].*png).*\(([0-9]{1,3}),\s+([0-9]{1,3}),\s+([0-9]{1,3}),\s+([0-9]{1,3})\)')
 GRIDS_PER_DIR = 8
 
 
@@ -41,68 +39,52 @@ def get_grids(img):
     return all_grids
 
 
-def generate_label_data(images, bb_collection):
+def generate_label_data(training_images, bb_collection):
     """
 
-    :param images: list of (image_file, numpy array)
+    :param training_images: list of (image_file, numpy array)
     :param bb_collection: list of (image_file, [(top_left, bottom_right)]
     :return:
     """
     label_data = {}
-    for img_file, img_data in images.items():
-        image_size = img_data[:, :, 0]
-        grid_width_x = image_size.shape[1] // GRIDS_PER_DIR
-        grid_width_y = image_size.shape[0] // GRIDS_PER_DIR
-
-        label = np.zeros((GRIDS_PER_DIR, GRIDS_PER_DIR))
-        grids = get_grids(img_data)
-        b_boxes = bb_collection[img_file]
-        # c = img_data.copy()
-        for box in b_boxes:
-            for grid in grids:
+    for img_file, img_data in training_images.items():
+        img_label = np.zeros((GRIDS_PER_DIR, GRIDS_PER_DIR))
+        img_grids = get_grids(img_data)
+        img_bounding_boxes = bb_collection[img_file]
+        for box in img_bounding_boxes:
+            for grid in img_grids:
                 if do_overlap(grid, box):
-                    # i = grid[0][1] // image_size.shape[1]#grid_width_y  # Y dir
-                    # j = grid[0][0] // #grid_width_x  # X dir
                     i = grid[1]
                     j = grid[0]
-                    label[i, j] = 1
-                    # cv2.rectangle(c, grid[0], grid[1], color=(34, 3, 3), thickness=-1)
-
-        label_data[img_file] = (grids, label)
-        # plt.subplot(2, 1, 1)
-        # plt.imshow(c)
-        # plt.title('A tale of 2 subplots')
-        # plt.ylabel('Damped oscillation')
-
-        # plt.subplot(2, 1, 2)
-        # plt.imshow(img_data)
-        # plt.show()
+                    img_label[i, j] = 1
+        label_data[img_file] = (img_grids, img_label)
     return label_data
 
 
 def read_bounding_boxes(bb_files):
-    bboxes = {}
+    all_bounding_boxes = {}
     for file in bb_files:
         with open(file) as bb_file:
-            for record in bb_file:
-                selected = FILE_EXTRACTION_RE.search(record).groups()
-                if  len(selected) == 5:
-                    if selected[0] is None or selected[1] is None:  # TODO (upul) check all
-                        msg = 'Record should contain both file name and bounding boxes'
-                        raise RuntimeError(msg)
+            for row in bb_file:
+                if 'xmin' in row:
+                    continue
+                else:
+                    cols = row.split(',')
+                    assert len(cols) == 7
+                    top_left = (int(cols[0]), int(cols[1]))
+                    bot_right = (int(cols[2]), int(cols[3]))
+                    frame = cols[4]
+                if frame in all_bounding_boxes:
+                    all_bounding_boxes[frame].append((top_left, bot_right))
+                else:
+                    all_bounding_boxes[frame] = [(top_left, bot_right)]
+    return all_bounding_boxes
 
-                    top_left = (int(selected[1]), int(selected[2]))
-                    bottom_right = (int(selected[3]), int(selected[4]))
-                    if selected[0].strip() in bboxes:
-                        bboxes[selected[0].strip()].append((top_left, bottom_right))
-                    else:
-                        bboxes[selected[0].strip()] = [(top_left, bottom_right)]
-    return bboxes
 
-
-def read_images(image_dirs, extension='png'):
+def read_images(image_dirs, ext='jpg'):
     """
 
+    :param ext: 
     :param image_dirs: list of directories which contains images
     :return: list of tuples, each tuple contains (image_file, numpy array)
     """
@@ -110,7 +92,7 @@ def read_images(image_dirs, extension='png'):
     for dir in image_dirs:
         files = os.listdir(dir)
         for file in files:
-            if extension in file:
+            if ext in file:
                 images[file] = plt.imread(os.path.join(dir, file))
     return images
 
@@ -120,31 +102,34 @@ def plot(image, grids, label, bbx):
         cv2.rectangle(image, grid[2], grid[3], color=(200, 10, 2), thickness=1)
 
     x_size, y_size = image.shape[1] // GRIDS_PER_DIR, image.shape[0] // GRIDS_PER_DIR
+    copy = image.copy()
     for j in range(GRIDS_PER_DIR):
         for i in range(GRIDS_PER_DIR):
             if label[i, j] == 1:
-                cv2.rectangle(image, (j * x_size, i * y_size), (j * x_size + x_size, i * y_size + y_size),
+                cv2.rectangle(copy, (j * x_size, i * y_size), (j * x_size + x_size, i * y_size + y_size),
                               color=(200, 1, 1), thickness=-1)
-
+    alpha = 0.2
+    cv2.addWeighted(copy, alpha, image, 1 - alpha, 0, image)
     grid = get_grids(image)
     for g in grid:
         cv2.rectangle(image, g[2], g[3], color=[100, 100, 50], thickness=2)
 
     for bb in bbx:
-        cv2.rectangle(image, bb[0], bb[1], color=[100, 50, 50], thickness=2)
+        cv2.rectangle(image, bb[0], bb[1], color=[100, 50, 50], thickness=3)
     plt.imshow(image)
     plt.show()
 
 
 if __name__ == '__main__':
-    image_dirs = ['./data/train-400']
-    annotation_files = ['./data/train-400/annotation.txt']
+    img_file_name = '1479498372942264998.jpg'
+    image_dirs = ['D:\\datasets\\udacity\\debug']
+    annotation_files = ['D:\\datasets\\udacity\\debug\\labels.csv']
     images = read_images(image_dirs)
     bboxes = read_bounding_boxes(annotation_files)
-    print(bboxes['carree-side-seq02-sideview_0195-02-h200-flipped.png'])
+    print(bboxes[img_file_name])
     labels = generate_label_data(images, bboxes)
-    grids = labels['carree-side-seq02-sideview_0195-02-h200-flipped.png'][0]
-    label = labels['carree-side-seq02-sideview_0195-02-h200-flipped.png'][1]
+    grids = labels[img_file_name][0]
+    label = labels[img_file_name][1]
     print(label)
-    plot(images['carree-side-seq02-sideview_0195-02-h200-flipped.png'], grids, label,
-         bboxes['carree-side-seq02-sideview_0195-02-h200-flipped.png'])
+    plot(images[img_file_name], grids, label,
+         bboxes[img_file_name])
